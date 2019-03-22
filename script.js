8674e55af407a784bff8d10c192ad9e8ebacee45
CBrown <47484591+caycebrown@users.noreply.github.com>
2019-03-22 15:40:53 -0500

    added if statements to fill in blanks when a matching keycode is entered

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-var theme = ["Sloth", "Chunk", "Mouth", "Mikey", "Data", "Brad", "Copperpot", "Willie", "Boondocks"]; //word bank
+var theme = [[83, 76, 79, 84,72], [67, 72, 85, 78, 75], [77, 79, 85, 84, 72], [77, 73, 75, 69, 89], [68, 65, 84, 65]]; //word bank
 
 var randomNumPick = Math.floor(Math.random(theme)*theme.length); //generates a random number no larger than the word theme list
 
 var wordPicker = theme[randomNumPick]; //uses randomly generated number to select a word from an array position
 
-var dash = ""; //creates empty string to be used for blank words
+var dash = []; //creates empty string to be used for blank words
 
 var guesses = [];
 
 var tries = 10;
+
+//takes the randomly selected word and creates a string with 1 dash/character
+function underScore() {
+    x = 0;
+    while (x < wordPicker.length) {
+    x += 1;
+    dash.push('_');
+
+    }
+
+    let content = document.getElementById('blankword');
+    content.textContent = dash.join(' ');
+
+};
 
 //This allows us to test for key presses that are alphabetical and add them to the guesses array
 document.onkeyup = function(event) {
@@ -31,25 +45,18 @@ document.onkeyup = function(event) {
         alert("Please enter a new letter key");
     }
 
-};
-
-
-
-//takes the randomly selected word and creates a string with 1 dash/character
-function underScore() {
-    x = 0;
-    while (x < wordPicker.length) {
-    x += 1;
-    dash = dash + '_ '
-
-    }
-
-    let content = document.getElementById('blankword');
-    content.textContent = dash;
+    if (wordPicker.includes(inputCode)) {
+        x = wordPicker.indexOf(inputCode);
+        dash[x] = (input);
+        let content = document.getElementById('blankword');
+        content.textContent = dash.join(' ');
+   };
 
 };
 
 
 //calling function -----> Need to update this to be called on button/key press
 underScore(); 
+
+
 
