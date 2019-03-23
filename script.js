72123cddb6b4ae62dff4e6ae5d513b9c93c95165
CBrown <47484591+caycebrown@users.noreply.github.com>
2019-03-22 21:23:27 -0500

    added stuff

--- script.js
+++ script.js
@@ -8,55 +8,67 @@ var dash = []; //creates empty string to be used for blank words
 
 var guesses = [];
 
-var tries = 10;
+tries = 8;
+
+
+
 
 //takes the randomly selected word and creates a string with 1 dash/character
-function underScore() {
-    x = 0;
-    while (x < wordPicker.length) {
-    x += 1;
-    dash.push('_');
+    function underScore() {
+        x = 0;
+        while (x < wordPicker.length) {
+        x += 1;
+        dash.push('_');
 
-    }
+        }
 
-    let content = document.getElementById('blankword');
-    content.textContent = dash.join(' ');
-
-};
-
-//This allows us to test for key presses that are alphabetical and add them to the guesses array
-document.onkeyup = function(event) {
-    inputCode = event.keyCode;
-    input = event.key;
-
-    if (inputCode >= 65 && inputCode <= 90) { 
-        validTest = (true);
-
-    }else {
-        validTest = (false);
-    };
-
-    if (validTest === (true) && guesses.includes(input) === (false)) {
-        tries -= 1;
-        guesses.push(input);
-        $('#guessBank').append(input + ', ');
-        
-    }else {
-        alert("Please enter a new letter key");
-    }
-
-    if (wordPicker.includes(inputCode)) {
-        x = wordPicker.indexOf(inputCode);
-        dash[x] = (input);
         let content = document.getElementById('blankword');
         content.textContent = dash.join(' ');
-   };
 
-};
+    };
+
+//This allows us to test for key presses that are alphabetical and add them to the guesses array
+    document.onkeyup = function(event) {
+        inputCode = event.keyCode;
+        input = event.key;
+
+        if (inputCode >= 65 && inputCode <= 90) { 
+            validTest = (true);
+
+        }else {
+            validTest = (false);
+        };
+
+        if (validTest === (true) && guesses.includes(input) === (false)) {
+            tries -= 1;
+            guesses.push(input);
+            $('#guessBank').append(input + ', ');
+        
+        }else {
+            alert("Please enter a new letter key");
+        }
+
+        if (tries === 0){
+            alert('you lose');
+        }else {
+
+        };
+
+        if (wordPicker.includes(inputCode)) {
+            x = wordPicker.indexOf(inputCode);
+            dash[x] = (input);
+            let content = document.getElementById('blankword');
+            content.textContent = dash.join(' ');
+        };
+
+    };
 
 
 //calling function -----> Need to update this to be called on button/key press
-underScore(); 
+    underScore(); 
+
+
+
 
 
 
