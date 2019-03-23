b557ec90d995cdfb27b31aab0b401ed957816fcb
CBrown <47484591+caycebrown@users.noreply.github.com>
2019-03-22 22:58:30 -0500

    added a reset button and a lose condition that stops onkeyup functionality

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var theme = [[83, 76, 79, 84,72], [67, 72, 85, 78, 75], [77, 79, 85, 84, 72], [77, 73, 75, 69, 89], [68, 65, 84, 65]]; //word bank
+var theme = [[83, 76, 79, 84,72], [67, 72, 85, 78, 75], [77, 79, 85, 84, 72], [77, 73, 75, 69, 89], [68, 65, 84, 65]]; //word bank by keycode
 
 var randomNumPick = Math.floor(Math.random(theme)*theme.length); //generates a random number no larger than the word theme list
 
@@ -8,12 +8,19 @@ var dash = []; //creates empty string to be used for blank words
 
 var guesses = [];
 
-tries = 8;
+$('#resetButton').click(function reset() {
+    dash = [];
+    guesses = [];
+    $('#guessBank').text(" ");
+    myFunction();
+});
 
 
 
+function myFunction() {
+    tries = 8;
 
-//takes the randomly selected word and creates a string with 1 dash/character
+    //creates blank word
     function underScore() {
         x = 0;
         while (x < wordPicker.length) {
@@ -27,47 +34,49 @@ tries = 8;
 
     };
 
-//This allows us to test for key presses that are alphabetical and add them to the guesses array
+     //This allows us to test for key presses that are alphabetical and add them to the guesses array
     document.onkeyup = function(event) {
-        inputCode = event.keyCode;
-        input = event.key;
+        if (tries > 0) {
 
-        if (inputCode >= 65 && inputCode <= 90) { 
-            validTest = (true);
+            inputCode = event.keyCode;
+            input = event.key;
 
+            if (inputCode >= 65 && inputCode <= 90) { 
+                validTest = (true);
+
+            }else {
+                validTest = (false);
+            };
+
+            if (validTest === (true) && guesses.includes(input) === (false)) {
+                tries -= 1;
+                guesses.push(input);
+                $('#guessBank').append(input + ', ');
+            
+            }else {
+                alert("Please enter a new letter key");
+            }
+
+            if (wordPicker.includes(inputCode)) {
+                x = wordPicker.indexOf(inputCode);
+                dash[x] = (input);
+                let content = document.getElementById('blankword');
+                content.textContent = dash.join(' ');
+            };
         }else {
-            validTest = (false);
-        };
-
-        if (validTest === (true) && guesses.includes(input) === (false)) {
-            tries -= 1;
-            guesses.push(input);
-            $('#guessBank').append(input + ', ');
-        
-        }else {
-            alert("Please enter a new letter key");
-        }
-
-        if (tries === 0){
-            alert('you lose');
-        }else {
-
-        };
-
-        if (wordPicker.includes(inputCode)) {
-            x = wordPicker.indexOf(inputCode);
-            dash[x] = (input);
-            let content = document.getElementById('blankword');
-            content.textContent = dash.join(' ');
+            document.onkeyup = null;
+            return alert("You Lost");
         };
 
     };
 
 
-//calling function -----> Need to update this to be called on button/key press
-    underScore(); 
+      //calling function -----> Need to update this to be called on button/key press
+    underScore();      
 
+};
 
+myFunction();
 
 
 
